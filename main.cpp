477c027a7a5f897f07636e894b6a2e772d1ef3a1
Nishai Kooverjee <Nishai@Nishais-MacBook.local>
2016-08-03 15:45:17 +0200

    Part 3: Animate sprite in main loop

--- main.cpp
+++ main.cpp
@@ -13,17 +13,32 @@ int main()
     // SpriteSheet Filename
     string spriteFilename = SPRITEFILENAME; // Leave this line
 
-    // Setup and Load Texture object here
+    Texture myTexture;
+    myTexture.loadFile(spriteFilename, 20, 20);
     bool quit = false;
+    int frame = 0;
 
     while(!quit){
         // Handle any SDL Events
         // Such as resize, clicking the close button,
         //  and process and key press events.
+        SDL_Event e;
 
-        // Update the Game State Information
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_QUIT){
+                quit = true;
+            }
+        }
 
-        // Draw the current state to the screen.
+        Tile pm(0,0,{ {1,1}, {1,2}, {1,1}, {1,3}},Pacman,1,1);
+
+        SDL_SetRenderDrawColor(myTexture.myWin.sdlRenderer, 0, 0, 0, 0xFF);
+        SDL_RenderClear(myTexture.myWin.sdlRenderer);
+        pm.render(&myTexture, frame);
+        SDL_RenderPresent(myTexture.myWin.sdlRenderer);
+
+        frame++;
+        this_thread::sleep_for(chrono::milliseconds(75));
     }
 
     return 0;
